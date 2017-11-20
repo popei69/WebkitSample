b49541c730f92d9d368e476ad520f9900cb43153
Benoit PASQUIER <b.pasquier69@gmail.com>
2017-11-20 21:36:40 +0000

    Sample UIViewController with Webkit handling javascript calls

--- WebkitSample/ViewController.swift
+++ WebkitSample/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKScriptMessageHandler {
+    
+    var webView: WKWebView!
+    
+    private func setupWebView() {
+        
+        let contentController = WKUserContentController()
+        let userScript = WKUserScript(
+            source: "mobileHeader()",
+            injectionTime: WKUserScriptInjectionTime.atDocumentEnd,
+            forMainFrameOnly: true
+        )
+        contentController.addUserScript(userScript)
+        contentController.add(self, name: "loginAction")
+        
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        self.webView = WKWebView(frame: self.view.bounds, configuration: config)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupWebView()
+        self.view.addSubview(self.webView)
+        
+        if let url = URL(string: "http://localhost:8888/web/webkitSample/index.html") { 
+            
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - WKScriptMessageHandler
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {       
+        if message.name == "loginAction" {
+            print("JavaScript is sending a message \(message.body)")
+        }
+    }
 }
-
